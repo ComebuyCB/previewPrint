4c7427a75e5a696b5052d366f5fa214b4f66d9d9
ComebuyCB <ben2000200066@yahoo.com.tw>
2022-01-28 21:56:44 +0800

    init

---
+++ static/common/js/main.js
@@ -0,0 +1,11 @@
+function _uuid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+function setFullScreenSize(target){
+  $(target).width( window.innerWidth )
+  $(target).height( window.innerHeight )
+}
