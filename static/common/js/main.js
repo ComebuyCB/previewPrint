84fddf791e02df3497de4ef4564ff9b37b2c6aa8
ComebuyCB <ben2000200066@yahoo.com.tw>
2022-02-08 16:40:05 +0800

    change to vue

--- static/common/js/main.js
+++ static/common/js/main.js
@@ -1,3 +1,11 @@
+set_100vh()
+window.onresize = function(){ set_100vh() }
+function set_100vh(){
+  let vh_100 = window.innerHeight;
+  document.documentElement.style.setProperty('--100vh', `${vh_100}px`);
+}
+
+
 function _uuid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
@@ -5,7 +13,6 @@ function _uuid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-function setFullScreenSize(target){
-  $(target).width( window.innerWidth )
-  $(target).height( window.innerHeight )
+function deepClone(obj){
+  return JSON.parse(JSON.stringify(obj))
 }
